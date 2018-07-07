42d10de88facc555f85ba2a9091cd26ee1516ff2
Kilian Kluge <kilian.kluge@gmail.com>
2018-07-07 16:08:55 +0200

    Trigger search on return key press

--- www/python/src/static/mitmachen.js
+++ www/python/src/static/mitmachen.js
@@ -19,7 +19,6 @@ $( function() {
         function(result) {
             var suggest = $("#suggested").text("Wie wäre es mit ")
             $.each(result.categories, function(i, value) {
-                console.log(value);
                 $("<span />").addClass("badge")
                     .addClass("badge-secondary").css("white-space", "normal")
                     .attr("id", "suggest-" + value).text(value).appendTo(suggest);
@@ -32,41 +31,42 @@ $( function() {
             suggest.append("?");
 
             $("span.badge").click(function(event){
-                console.log(event.target.id);
                 $("#category").val(event.target.id.replace(/suggest-/, ""));
                 $("#category").change();
             });
     });
 
-    $("#category").change(function(event) {
-        var topic = $("#category").val();
-        console.log(topic);
+    $("#category").on("change keypress", function(event) {
+        if (e.type == 'change' || (e.type == 'keypress' && e.which == 13)) {
+            var topic = $("#category").val();
+            console.log(topic);
 
-        var articleList = $("#articles");
-        articleList.empty();
-
-        $("<li/>").addClass("list-group-item")
-            .text("Bitte warten...").appendTo(articleList);
-
-
-        $.getJSON($URL_FOR_FIND, {q: topic}, function(result) {
+            var articleList = $("#articles");
             articleList.empty();
-            $.each(result.articles, function(i, doc) {
-                var li = $("<li/>").addClass("list-group-item").appendTo(articleList);
-                var div = $("<div/>").appendTo(li);
-                var htitle = $("<h6>").addClass("my-0").appendTo(div);
 
-                $.getJSON("https://de.wikipedia.org/api/rest_v1/page/summary/".concat(encodeURIComponent(doc.page)),
-                function(result) {
-                    $("<a/>").attr("href", "https://de.wikipedia.org/wiki/".concat(encodeURIComponent(doc.page))).html(result.displaytitle).appendTo(htitle);
-                    $("<small/>").addClass("text-muted").text(result.description).appendTo(div);
-                });
+            $("<li/>").addClass("list-group-item")
+                .text("Bitte warten...").appendTo(articleList);
 
-                $.each(doc.problems, function(i, problem){
-                    $("<span/>").addClass("badge").addClass("badge-warning").text(problem).appendTo(li);
+
+            $.getJSON($URL_FOR_FIND, {q: topic}, function(result) {
+                articleList.empty();
+                $.each(result.articles, function(i, doc) {
+                    var li = $("<li/>").addClass("list-group-item").appendTo(articleList);
+                    var div = $("<div/>").appendTo(li);
+                    var htitle = $("<h6>").addClass("my-0").appendTo(div);
+
+                    $.getJSON("https://de.wikipedia.org/api/rest_v1/page/summary/".concat(encodeURIComponent(doc.page)),
+                    function(result) {
+                        $("<a/>").attr("href", "https://de.wikipedia.org/wiki/".concat(encodeURIComponent(doc.page))).html(result.displaytitle).appendTo(htitle);
+                        $("<small/>").addClass("text-muted").text(result.description).appendTo(div);
+                    });
+
+                    $.each(doc.problems, function(i, problem){
+                        $("<span/>").addClass("badge").addClass("badge-warning").text(problem).appendTo(li);
+                    });
                 });
             });
-        });
-
+        }
     });
+
 });
