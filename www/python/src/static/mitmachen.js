5b366f269271bd045a3b01a3f1b562744b2f1f78
Kilian Kluge <kilian.kluge@gmail.com>
2018-07-09 19:54:57 +0200

    Show button to load more articles, suggest new list of articles when no results were found

--- www/python/src/static/mitmachen.js
+++ www/python/src/static/mitmachen.js
@@ -15,26 +15,29 @@ $( function() {
         }
     });
 
-    $.getJSON($URL_FOR_SUGGEST, {},
-        function(result) {
-            var suggest = $("#suggested").text("Wie wäre es mit ")
-            $.each(result.categories, function(i, value) {
-                $("<span />").addClass("badge")
-                    .addClass("badge-secondary").css("white-space", "normal")
-                    .attr("id", "suggest-" + value).text(value).appendTo(suggest);
-                if (i < result.categories.length - 2) {
-                    suggest.append(" ");
-                } else if ( i == result.categories.length - 2) {
-                    suggest.append(" oder ")
-                }
-            });
-            suggest.append("?");
+    function suggest_topics() {
+        $.getJSON($URL_FOR_SUGGEST, {},
+            function(result) {
+                var suggest = $("#suggested").text("Wie wäre es mit ")
+                $.each(result.categories, function(i, value) {
+                    $("<span />").addClass("badge")
+                        .addClass("badge-secondary").css("white-space", "normal")
+                        .attr("id", "suggest-" + value).text(value).appendTo(suggest);
+                    if (i < result.categories.length - 2) {
+                        suggest.append(" ");
+                    } else if ( i == result.categories.length - 2) {
+                        suggest.append(" oder ")
+                    }
+                });
+                suggest.append("?");
 
-            $("span.badge").click(function(event){
-                $("#category").val(event.target.id.replace(/suggest-/, ""));
-                $("#category").change();
-            });
-    });
+                $("span.badge").click(function(event){
+                    $("#category").val(event.target.id.replace(/suggest-/, ""));
+                    $("#category").change();
+                });
+        });
+    }
+    suggest_topics();
 
     $("#category").on("change keypress", function(event) {
         if (event.type == 'change' || (event.type == 'keypress' && event.which == 13)) {
@@ -51,32 +54,49 @@ $( function() {
             $.getJSON($URL_FOR_FIND, {q: topic}, function(result) {
                 articleList.empty();
                 $.each(result.articles, function(i, doc) {
-                    var li = $("<li/>").addClass("list-group-item").appendTo(articleList);
+                    var li = $("<li/>").addClass("list-group-item")
+                                       .appendTo(articleList);
                     var div = $("<div/>").appendTo(li);
                     var htitle = $("<h6>").addClass("my-0").appendTo(div);
 
                     $.getJSON("https://de.wikipedia.org/api/rest_v1/page/summary/".concat(encodeURIComponent(doc.page)),
                     function(result) {
-                        $("<a/>").attr("href", "https://de.wikipedia.org/wiki/".concat(encodeURIComponent(doc.page))).html(result.displaytitle).appendTo(htitle);
-                        $("<small/>").addClass("text-muted").text(result.description).appendTo(div);
+                        $("<a/>").attr("href", "https://de.wikipedia.org/wiki/".concat(encodeURIComponent(doc.page)))
+                                 .html(result.displaytitle)
+                                 .appendTo(htitle);
+                        $("<small/>").addClass("text-muted")
+                                     .text(result.description)
+                                     .appendTo(div);
                     });
 
                     $.each(doc.problems, function(i, problem){
-                        $("<span/>").addClass("badge").addClass("badge-warning").text(problem).appendTo(li);
+                        $("<span/>").addClass("badge")
+                                    .addClass("badge-warning")
+                                    .text(problem)
+                                    .appendTo(li);
                         li.append(" ");
                     });
                 });
 
                 if (result.articles.length == 0) {
                     $("<li/>").addClass("list-group-item")
-                        .text("Keine Ergebnisse gefunden. Wie wäre es mit einem der nebenstehenden Themen?")
-                        .appendTo(articleList);
+                              .text("Keine Ergebnisse gefunden. Wie wäre es mit einem der nebenstehenden Themen?")
+                              .appendTo(articleList);
+                    suggest_topics();
                 }
 
                 if (result.more) {
-                    $("<li/>").addClass("list-group-item")
-                        .text("Weitere Ergebnisse verfügbar.")
-                        .appendTo(articleList);
+                    var more = $("<li/>").addClass("list-group-item")
+                                         .text("Weitere Ergebnisse verfügbar.")
+                                         .appendTo(articleList);
+                    $("<button />").addClass("btn")
+                                   .addClass("btn-secondary")
+                                   .attr("type", "button")
+                                   .text("Neue Auswahl")
+                                   .click(function(event) {
+                                       $("#category").change();
+                                   })
+                                   .appendTo(more);
                 }
             });
         }
