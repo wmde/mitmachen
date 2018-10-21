e8b773db2ac3923d693af14f938debf2782b7109
Kilian Kluge <kilian.kluge@wikipedia.de>
2018-10-21 12:14:10 +0000

    Introduce intro text

--- www/python/src/text.js
+++ www/python/src/text.js
@@ -1,6 +1,7 @@
 {
     "TITLE": "Mitmachen bei Wikipedia",
-    "CLAIM": "Bei Wikipedia gibt es immer was zu tun... Hier findest du kleine und größere Aufgaben in allen möglichen Wissensgebieten. Jedes Komma zählt - mit deinem Beitrag verbesserst du die größte Enzyklopädie der Welt für Millionen von Menschen!",
+    "CLAIM": "Bei Wikipedia gibt es immer was zu tun...",
+    "INTRO_TEXT": "Hier findest du kleine und größere Aufgaben in allen möglichen Wissensgebieten. Jedes Komma zählt - mit deinem Beitrag verbesserst du die größte Enzyklopädie der Welt für Millionen von Menschen!",
     "INTRO_QUESTION": "In welchem Themengebiet kennst du dich aus?",
     "SUGGEST_LOADING": "Lade Vorschläge...",
     "SUGGEST_RELOAD": "Neue Vorschläge"
