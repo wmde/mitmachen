44c5ad5eb97aa57b35579db7cbd08216b6c98592
Deepak <dev.ckln@gmail.com>
2019-09-02 15:48:28 +0530

    suggestion query update

--- www/python/queries/suggest.sql
+++ www/python/queries/suggest.sql
@@ -1,8 +1,13 @@
-SELECT
-	cat_title
-FROM
-	category
-WHERE
-    cat_pages > 50
-ORDER BY RAND()
+SELECT 
+	cat.cat_title, COUNT(catlink.cl_timestamp) as count
+FROM 
+	category as cat 
+INNER JOIN 
+	categorylinks as catlink 
+ON 
+	cat.cat_title = catlink.cl_to 
+WHERE 
+	cat.cat_pages > 50 
+GROUP BY cat.cat_title
+ORDER BY RAND() 
 LIMIT 10;
