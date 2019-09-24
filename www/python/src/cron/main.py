0da371119062cbb1a0dd1c3efb233e4f7ebbc131
Deepak <dev.ckln@gmail.com>
2019-09-24 10:16:21 +0530

    cron job code

---
+++ www/python/src/cron/main.py
@@ -0,0 +1,35 @@
+import urllib.request, json
+import toolforge
+
+conn = toolforge.connect("s53772__mitracking_p", cursorclass=pymysql.cursors.DictCursor)
+
+# having issue with kunst and kultur category, can't get result with space and underscore in between
+all_links = {
+	"Geographie": "https://petscan.wmflabs.org/?psid=11395957&format=json",
+	"Geschichte": "https://petscan.wmflabs.org/?psid=11395982&format=json",
+	"Gesellschaft": "https://petscan.wmflabs.org/?psid=11396009&format=json",
+	"Religion": "https://petscan.wmflabs.org/?psid=11396021&format=json",
+	"Sport": "https://petscan.wmflabs.org/?psid=11396048&format=json",
+	"Technik": "https://petscan.wmflabs.org/?psid=11396099&format=json",
+	"Wissen": "https://petscan.wmflabs.org/?psid=11396124&format=json",
+	"Kunst_und_Kultur": "https://petscan.wmflabs.org/?psid=11397537&format=json"
+}
+
+for item in list(all_links.keys()):
+	with urllib.request.urlopen(all_links[item]) as url:
+		data = json.loads(url.read().decode())
+		
+		query = "INSERT INTO crondata (categ, subcateg, page_id) VALUES " + ",".join("(%s, %s, %s)" for _ in len(data['*']['a']['*']))
+		val_insert = [(item, elem['title'].lower(), elem['id']) for elem in data['*']['a']['*']]
+		flatten_values = [pp for sublist in val_insert for pp in sublist]
+
+		try:
+			with conn.cursor() as cursor:
+				cursor.execute(query, flatten_values)
+
+				conn.commit()
+		except Exception as e:
+			print ('Error with insert query: ', e)
+		finally:
+			# conn.close()
+			print ('Done with: ', item)
