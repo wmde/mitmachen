b31a454ad8d0da8104c80171228ddf497e2c3f41
Deepak <dev.ckln@gmail.com>
2019-10-10 12:01:56 +0530

    hide show changes

--- www/python/src/static/new/js/mitmachen.js
+++ www/python/src/static/new/js/mitmachen.js
@@ -307,6 +307,7 @@ function findTopics(topic){
                 /*$("<small/>").addClass("text-muted")
                     .text(result.description)
                     .appendTo(div);*/
+                $("<span/>").text(result.description).appendTo(ptag);
                 var pvLink = $URL_FOR_PREVIEW + "?q=https://de.wikipedia.org/wiki/".concat(encodeURIComponent(doc.page)) + "&problems=".concat(doc.problems);
                     
                 $("<a/>").attr("href", pvLink).attr('target', '_blank').html("Preview Article").appendTo(ptag);
