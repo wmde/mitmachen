e1b3f35b3ab11c88bf74c0986d7160df2bb28b48
Deepak <dev.ckln@gmail.com>
2019-11-01 12:48:02 +0530

    Translate pending

--- www/python/src/static/new/js/mitmachen.js
+++ www/python/src/static/new/js/mitmachen.js
@@ -430,7 +430,7 @@ function runTabAndCategCode(t){
         var fs = curSelUserInterests.length > 0 ? curSelUserInterests.slice(0,2).join(", ") : "";
         var ls = curSelUserInterests.length > 0 ? curSelUserInterests.slice(2).length : "";
 
-        var textToAdd = (curSelUserInterests != undefined && curSelUserInterests.length > 0) ? ls > 0 ? (fs + " +"+ls+" more <a href='#' class='edit-categ' data-toggle='modal' data-target='#changeCateg'>Auswahl ändern</a>") : fs + " <a href='#' class='edit-categ' data-toggle='modal' data-target='#changeCateg'>Auswahl ändern</a>" : ("No interests selected, please select. <a href='#' class='edit-categ' data-toggle='modal' data-target='#changeCateg'>Auswahl ändern</a>");
+        var textToAdd = (curSelUserInterests != undefined && curSelUserInterests.length > 0) ? ls > 0 ? (fs + " +"+ls+" more <a href='#' class='edit-categ' data-toggle='modal' data-target='#changeCateg'>Auswahl ändern</a>") : fs + " <a href='#' class='edit-categ' data-toggle='modal' data-target='#changeCateg'>Auswahl ändern</a>" : ("Keine Interessen ausgewählt, bitte auswählen. <a href='#' class='edit-categ' data-toggle='modal' data-target='#changeCateg'>Auswahl ändern</a>");
         $('.user-sel-categories').html(textToAdd);
 
         getSubcategoriesForUser('user_interests');
